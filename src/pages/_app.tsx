963fd1d519717886564df4f8e3181d7351759a80
guilhermeft <guilhermetrind12@gmail.com>
2022-09-23 00:17:31 -0300

    feat: adding top progressBar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, useTheme } from 'styled-components'
 
 import GlobalStyles from '@/styles/global'
 import { darkTheme } from '@/styles/themes'
@@ -9,6 +10,8 @@ import MainLayout from '@/templates/Mainlayout'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
+      <NextNProgress height={1} color={darkTheme.colors.blue[200]} />
+
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
