7246be9654f8deef1fb293d7d800c43616897cee
GuilhermeFT <guilhermetrind12@gmail.com>
2024-12-03 00:47:34 -0300

    fix: change lib

--- src/components/icons/mouse-icon.tsx
+++ src/components/icons/mouse-icon.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import { twMerge } from 'tailwind-merge'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 
 type IconProps = {
   className?: string
@@ -10,7 +10,7 @@ type IconProps = {
 export function MouseIcon({ className }: IconProps) {
   return (
     <svg
-      className={twMerge('w-5 h-5', className)}
+      className={twMerge('h-5 w-5', className)}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="none"
